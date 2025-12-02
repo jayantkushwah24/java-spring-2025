989360804237d47a373f23b21a8f3e7604ad2bf5
Jayant Kushwah <jayantkushwah8@gmail.com>
2025-12-02 10:28:02 +0530

    lecture 2 completed

---
+++ intro to java-2/variable.java
@@ -0,0 +1,33 @@
+
+public class variable {
+
+  public static void main(String[] args) {
+    // integer types
+    byte age = 25;
+    short year = 2025;
+    int salary = 90000;
+    long mobileNo = 8770443828L;
+
+    // floating
+    float payment = 124.54f;
+    double bigPay = 243454534.23d;
+
+    // boolean
+    boolean isLegal = true;
+
+    // char
+    char grade = 'A';
+
+    String name = "jayant";
+
+    System.out.println("name : " + name);
+    System.out.println("age : " + age);
+    System.out.println("year : " + year);
+    System.out.println("salary : " + salary);
+    System.out.println("mobile number : " + mobileNo);
+    System.out.println("payment : " + payment);
+    System.out.println("big Pay : " + bigPay);
+    System.out.println("is legal? : " + isLegal);
+    System.out.println("grade : " + grade);
+  }
+}
